9d1559afa2e78413d8059d754291dc0fd523ac43
Darian <darian@arbitraryexecution.com>
2022-03-01 14:31:46 -0800

    implmented coingecko api to get price of tokens

--- liquidity-pool-changes/src/agent.js
+++ liquidity-pool-changes/src/agent.js
@@ -1,4 +1,6 @@
-const BigNumber = require("bignumber.js");
+const BigNumber = require("bignumber.js"); // always convert ethers.js bignumber to javascript bignumber
+const axios = require("axios").default;
+
 const {
   Finding,
   FindingSeverity,
@@ -11,6 +13,8 @@ const {
 const config = require("../agent-config.json");
 
 const utils = require("./utils");
+
+const DECIMALS_ABI = ["function decimals() view returns (uint8)"];
 
 // used to store initialization data once you pass it into the provideHandleBlock() function
 const initializeData = {};
@@ -55,18 +59,60 @@ function provideHandleBlock(data) {
     // make sure that data is initialized first
     if (!provider) throw new Error("handleBlock called before initialization");
 
+    const findings = [];
+
     let poolContract = utils.getContract("Usdc/EthPool", provider);
-    let token0Address = await poolContract.functions.token0();
-    let token1Address = await poolContract.functions.token1();
+    let token0Address = await poolContract.token0();
+    let token1Address = await poolContract.token1();
 
     let token0Contract = new ethers.Contract(token0Address, erc20Abi, provider);
     let token1Contract = new ethers.Contract(token1Address, erc20Abi, provider);
 
+    // attatch erc20 methods so you can check the erc20 token balances of each token for the given pool
     token0Contract = token0Contract.attach(token0Address);
-    token1Contract = token1Contract.attatch(token1Address);
+    token1Contract = token1Contract.attach(token1Address);
+
+    let balance = await token0Contract.balanceOf(poolContract.address);
+    // factoryContract.getPool(token0Address, token1Address, )
 
     // get a block and filter it through to test it out
+    let result = await getTokenPrices(token0Address, token1Address);
+
+    // return findings at the end
+    return findings
   };
+}
+
+async function getTokenPrices(token0Address, token1Address) {
+  const coingeckoApiUrl =
+    "https://api.coingecko.com/api/v3/simple/token_price/ethereum?";
+  const addressQuery = `contract_addresses=${token0Address},${token1Address}`;
+  const vsCurrency = "&vs_currencies=usd";
+
+  const url = coingeckoApiUrl.concat(addressQuery.concat(vsCurrency));
+  const { data } = await axios.get(url);
+
+  // parse response to convert to BigNumber.js
+  const usdPerToken0 = new BigNumber(data[token0Address.toLowerCase()].usd);
+  const usdPerToken1 = new BigNumber(data[token1Address.toLowerCase()].usd);
+
+  return { token0Price: usdPerToken0, token1Price: usdPerToken1 };
+}
+
+async function getSwapTokenUSDValue() {
+  // get the decimal scaling for this token
+  const contract = new ethers.Contract(tokenAddress, DECIMALS_ABI, provider);
+  let decimals;
+  try {
+    // calling .decimals() may fail for a vyper contract
+    decimals = await contract.decimals();
+  } catch {
+    return undefined;
+  }
+  const denominator = new BigNumber(10).pow(decimals);
+
+  // multiply by the price and divide out decimal places
+  return amountBN.times(tokenPrice).div(denominator);
 }
 
 // helper function to create alerts
